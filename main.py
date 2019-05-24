a7b5c61c09f5cb2c7be3b2d8bd689cbdd5550c15
Abdullah <alrashidiaa@uindy.edu>
2019-05-24 02:50:29 -0400

    specifing what is going to be in main

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-Class Main():
-    
+def Main():
+
+    # in this Function the main functionality of the program will be
+    # the users will be asked for his Credntials
+    # all other classes will be connected to this class
+    # there will be a while loop that takes the users clothes type
+
+
+
+if __name__ == '__main__':
+    Main()
