ef75956fe34f02ef0f921b84bf89dc016a6cfabe
Abdullah <alrashidiaa@uindy.edu>
2019-06-01 15:51:11 -0400

    adding menu for user

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 def Main():
 
-    # in this Function the main functionality of the program will be
-    # the users will be asked for his Credntials
-    # all other classes will be connected to this class
-    # there will be a while loop that takes the users clothes type
-    # TEST
+    raw_input("Enter your username")
+    raw_input("Enter your password")
+
+    #here we will send Credientials to login function
+    #If login successful user can see the menu 
+
+    def Menu():
+        print "Welcome to AJ Laundry"
+        print "1. Enter clothes to be washed"
+        print "2. Show Reciept"
+        print "3. Exit"
+
+    loop = True
+
+    while loop:
+        print Menu()
+        choice = input("Enter your choice: ")
+
+        if choice ==1:
+            #will ask user for input
+            print("choice 1 selected: ")
+
+        elif choice ==2:
+            #will show the user the reciept
+            print("choice 2 selected")
+
+        elif choice ==3:
+            loop = False
+
+        else:
+            raw_input("Wrong choice, Enter a valid choice")
+
 
 
 
