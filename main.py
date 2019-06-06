5f0d66b8baa73829676cc0fd3c11e88fe0dd685b
Abdullah <alrashidiaa@uindy.edu>
2019-06-05 21:21:32 -0400

    Taking user Input and displaying part of the reciept

--- main.py
+++ main.py
@@ -20,11 +20,39 @@ def Main():
 
         if choice ==1:
             #will ask user for input
-            print("choice 1 selected: ")
+
+            loop1 = True
+            while loop1:
+
+
+                Clothes = raw_input("Enter clothes you want to wash (when finish enter e): ")
+                if Clothes == 'short':
+                    shortCount = input("how many shorts: ")
+                if Clothes == 'tshirt':
+                    tshirtCount = input("how many tshirts: ")
+                if Clothes == 'socks':
+                    socksCount = input("how many socks: ")
+                if Clothes == "pillow case":
+                    pillowCaseCount = input("how many pillow cases: ")
+                if Clothes == "Jacket":
+                    jacketCount = input("how many Jakcets: ")
+                if Clothes == 'e':
+                    loop1 = False
+
+                #After that we will send input of clothes count to a function that handle the prices
+
+
+
 
         elif choice ==2:
             #will show the user the reciept
-            print("choice 2 selected")
+            print("---------------Recipet-------------------")
+            print("short:        " + shortCount)
+            print("tshirts:      " + tshirtCount)
+            print("Socks:        " + socksCount)
+            print("pillow cases: " + pillowCaseCount)
+            print("Jackets:      " + jacketCount)
+
 
         elif choice ==3:
             loop = False
