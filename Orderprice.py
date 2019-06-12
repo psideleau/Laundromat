b80677615d95220aec1dbc3cc87c119b578251a9
Gold Wejinya <goldwejinya@galleryxhibit44.home>
2019-06-12 03:28:31 -0400

    orderprice

--- Orderprice.py
+++ Orderprice.py
@@ -1,2 +1,16 @@
-class Orderprice:
+class orderprice:
+    def dropTIme(self):  
+        print ("Enter a dropoff time between 900hours and 1500hours:")
+        droptime = raw_input("> ")
+
+        if droptime in range(9, 3):
+            print ("Drop time of " ,droptime, "hours OK")
+
+        else:
+            print ("Invalid Drop time")
+        return droptime
+
     
+    def comment(self):
+        comment = input("Enter any other information about your items: ")
+        return comment
