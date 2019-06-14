689e63e0fa8da7d7b15fbd76ede29a12085ae1c1
Suzan Ntanyi <suzanntanyi@Suzans-MacBook-Air.local>
2019-06-14 18:58:21 -0400

    Database

--- Database1.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('my_database.sqlite')
-cursor = conn.cursor()
-print("Opened database successfully")
-
--- Database2.py
+++
@@ -1,7 +0,0 @@
-cursor.execute('''CREATE TABLE LAUNDROMART SERVICES
-         (ID INT PRIMARY KEY     NOT NULL,
-         FIRST NAME           TEXT    NOT NULL,
-         LAST NAME           TEXT     NOT NULL,
-         EMAIL ADDRESS       CHAR(50),
-         POINTS          INT);''')
-cursor.close()
