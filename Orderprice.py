18d0f93fa2af12080fe40137ab6a415b048f8f3c
Gold Wejinya <goldwejinya@Gold's macbook.home>
2019-06-15 19:21:38 -0400

    orderprice update

--- Orderprice.py
+++ Orderprice.py
@@ -1,36 +1,38 @@
 class Orderprice:
-<<<<<<< HEAD
-    def Totaldue(self, Total):
-        Total
         
-=======
->>>>>>> a97658c62a1aa16982cbe95623af50ece80cabb4
-    def dropTIme(self, droptime):
-        if droptime in range(9, 15):
-            print ("Drop time of " ,droptime, "hours OK")
-        else:
-            print ("Invalid Drop time")
-        return droptime
+        def totaldue(self,total):
+            self.Totaldue = total
 
-    def expedite(self, expedite):
-        if expedite == 1:
-            estimatedtime = droptime + 3
-            Total = Total * 2
-        else:
-            estimatedtime = droptime + 7
-        return estimatedtime, Total
+        def droptime(self, droptime):
+            self.droptime = droptime
+                
+            if droptime in range(9, 15):
+                print ("Drop time of ", droptime, "hours OK")
+            else:
+                print ("Invalid Drop time")
+            
+        def expedite(self, expedite):
+            if expedite == 1:
+                self.estimatedtime = self.droptime +3
+                self.Totaldue = self.Totaldue * 2
+            else:
+                self.estimatedtime = self.droptime + 7
+            
+        def prefume(self, perfume):
+            if perfume == 1:
+                self.Totaldue = self.Totaldue + 3
+            else:
+                self.Totaldue = self.Totaldue
+            
+        def iron(self, ironing):
+            if ironing == 1:
+                self.Totaldue = self.Totaldue+3
+            else:
+                self.Totaldue = self.Totaldue
+       
+        
 
-    def perfume(self, perfume):
-        if perfume == 1:
-            Total = Total + 3
-        else:
-            Total = Total
-
-    def ironing(self, ironing):
-        if ironing == 1:
-
-        else
-    
-    def comment(self, comment):
-        comment = input("Enter any other information about your items: ")
-        return comment
+        def printTotaldue(self):
+            print("Total price = ", self.Totaldue)
+            print("Estimated Time = ", self.estimatedtime)
+            print("drop time = ", self.droptime)
