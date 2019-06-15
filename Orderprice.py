d491bce69df3b01f55a725edf6cbcb43c025d8b4
Gold Wejinya <goldwejinya@Gold's macbook.home>
2019-06-15 12:47:10 -0400

    update

--- Orderprice.py
+++ Orderprice.py
@@ -1,5 +1,4 @@
-class orderprice:
-    class Orderprice:
+class Orderprice:
     def dropTIme(self, droptime):
         if droptime in range(9, 15):
             print ("Drop time of " ,droptime, "hours OK")
@@ -7,7 +6,18 @@ class orderprice:
             print ("Invalid Drop time")
         return droptime
 
+    def expedite(self, expedite):
+        if expedite == 1:
+            Totaldue = Totaldue * 3
+        else:
+            
+
+    def perfume(self, perfume):
+        if perfume == 1:
+
+        else:
+            
     
-    def comment(self):
+    def comment(self, comment):
         comment = input("Enter any other information about your items: ")
         return comment
