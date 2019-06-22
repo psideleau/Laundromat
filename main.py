0e1554ebbf9444d58f58cd9b769353df5f49fa30
Gold Wejinya <goldwejinya@Gold's macbook.home>
2019-06-21 22:05:48 -0400

    final update

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def Main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            tshirtCount = "0"
-            shortCount = "0"
-            socksCount = "0"
-            pillowCaseCount = "0"
-            jacketCount = "0"
+            tshirtCount = 0
+            shortCount = 0
+            socksCount = 0
+            pillowCaseCount = 0
+            jacketCount = 0
             
             #will ask user for input
 
@@ -44,16 +44,21 @@ def Main():
                 if Clothes == 'e':
                     loop1 = False
 
-            exedite = int(input("Do you want expedited cleaning or normal (enter 1 for expedite 2 for normal"))
-            iron = int(input("Enter 1 for ironing 2 for no ironing"))
-            prefume = int(input("Enter 1 for prefume 2 for no"))
-            droptime = int(input("Enter Drop time "))
-            comment = input("Enter any other information about your items: ")
+            exedite = int(input("Do you want expedited cleaning or normal (enter 1 for expedite 2 for normal=> "))
+            iron = int(input("Enter 1 for ironing 2 for no ironing => " ))
+            prefume = int(input("Enter 1 for prefume 2 for no => "))
+            droptime = int(input("Enter Drop time => "))
+            comment = input("Enter any other information about your items=> ")
 
                 #After that we will send input of clothes count to a function that handle the prices
                 #After geting the cleaning price we will send the choices to a function
                 #that will calculate the time will take for cleaning the clothes and final price
-            total = 20
+            shortcost = shortCount * 2
+            tshirtcost = tshirtCount * 2
+            sockscost = socksCount * 1
+            pillowcost = pillowCaseCount * 2
+            jacketcost = jacketCount * 2
+            total = shortcost + tshirtcost +sockscost + pillowcost + jacketcost
             orderprice = Orderprice()
             orderprice.totaldue(total)
             orderprice.droptime(droptime)
@@ -70,12 +75,13 @@ def Main():
             #Here we will get the prices from the function that calaculate the prices
             #Then, we will show the user the reciept
             print("---------------Recipet-------------------")
-            print("short:        " , shortCount)
-            print("tshirts:      " , tshirtCount)
-            print("Socks:        " , socksCount)
-            print("pillow cases: " , pillowCaseCount)
-            print("Jackets:      " , jacketCount)
+            print("short:        " , shortCount,      "---------> $", shortcost)
+            print("tshirts:      " , tshirtCount,    "---------> $", tshirtcost)
+            print("Socks:        " , socksCount,     "---------> $", sockscost)
+            print("pillow cases: " , pillowCaseCount, "---------> $", pillowcost)
+            print("Jackets:      " , jacketCount,     "---------> $", jacketcost)
             print("***************************************")
+            
             orderprice.printTotaldue()
 
 
